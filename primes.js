d9233fb1c8a06f3e5e9fb97b0043f5f45cdb9bed
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2015-12-17 09:14:30 +0000

    Create primes.js

---
+++ primes.js
@@ -0,0 +1,17 @@
+// Find the maximum value in a list of numbers
+var num = [];
+numb = function(a) {
+    var n = 30
+    for (var i = 0; i < n; i++) {
+        writeln("n = "+i);
+        num[i] = i;
+    }
+}
+numb()
+writeln(num)
+pri = function(nu) {
+    for (var j = 0; j < nu.length; j++) {
+        writeln("n = "+i);
+        num[i] = i;
+    }
+}
