4596ab19fd3f0141800a0477cf885ac672daba74
Gum-Joe <kishansambhi@hotmail.co.uk>
2016-01-06 20:21:29 +0000

    No more cheats

--- primes.js
+++ primes.js
@@ -1,29 +1,56 @@
-// Find the maximum value in a list of numbers
-var num = [];
-var prim = [];
-var n = 30
-numb = function(a) {
-    for (var i = 0; i < n; i++) {
-        num[i] = i;
+// Find all the primes below a number
+// Basic vars
+var num = 10;
+nump = num + 1
+var prims = [];
+var nprims = [];
+var fprims = [];
+var fs = require('fs');
+var npr = []
+
+// Nodejs
+// writeln = console.log;
+fs.openSync("log.log", "w");
+// Get all numbers before
+for (var i = 0; i < nump; i++) {
+  // Check whether i is divisible by each number below it
+  //writeln(i);
+  for (var p = 0; p < i; p++) {
+    if (i === 0 || i === 1 || i === p || p === 1) {
+
+    } else {
+      if (i % p === 0) {
+        // Add to not prime array if not a prime
+        npr.push(i)
+      }
     }
-}
-numb()
-// Prime numbers up-to 40
-pr = [1,2,3,5,7,11,13,17,19,23,29,31,37,39,41,43,47,53,59,61,67,71,73,79,83,89,97]
-pri = function(nu) {
-    for (var j = 0; j < num.length; j++) {
-        for (var a = 0; a < pr.length; a++) {
-          if (num[j] === pr[a]) {
-            prim[a] = num[j]
-          }
-        }
-    }
+  }
 }
 
-pri()
+// Go through numbers and work out if prime
+for (var a = 0; a < num; a++) {
+  prims.push(a);
+  for (var b = 0; b < npr.length; b++) {
+    if (npr[b] == a) {
+      if (prims[prims.length-1] == a) {
+        prims[prims.length-1] = null;
+      }
+    }
+  }
+}
 
-writeln("There are "+prim.length+" prime numbers before "+n)
+// Print
+// get final array
+for (var c = 0; c < prims.length; c++) {
+  if (prims[c] !== null && prims[c] !== 0) {
+    fprims.push(prims[c]);
+  }
+}
+
+writeln("There are "+fprims.length+" before the number "+num)
 writeln("They are:");
-for (var b = 0; b < prim.length; b++) {
-  writeln(prim[b]);
+for (var x = 0; x < fprims.length; x++) {
+  if (fprims[x] !== null) {
+    writeln(fprims[x]);
+  }
 }
